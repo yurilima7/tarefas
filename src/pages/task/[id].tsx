8e3689224dbab14eb7a003f6e6fc9ac6da16ccdd
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-07-28 18:41:28 -0300

    detalhes da tarefa

---
+++ src/pages/task/[id].tsx
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import styles from "./styles.module.css";
+import { GetServerSideProps } from "next";
+import { db } from "../../services/firebase_connection";
+import { collection, query, where, doc, getDoc } from "firebase/firestore";
+import { Textarea } from '@/components/textarea/textarea';
+
+interface TaskProps {
+    item: {
+        tarefa: string;
+        created: string;
+        public: boolean;
+        user: string;
+        taskId: string;
+    }
+}
+
+export default function Task({ item }: TaskProps) {
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Detalhes da tarefa</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1>Tarefa</h1>
+
+                <article className={styles.task}>
+                    <p>{item.tarefa}</p>
+                </article>
+            </main>
+
+            <section className={styles.commentsContainer}>
+                <h2>Deixar comentário</h2>
+
+                <form >
+                    <Textarea 
+                        placeholder="Digite seu comentário..."
+                    />
+
+                    <button className={styles.button}>Enviar comentário</button>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const id = params?.id as string;
+
+    const docRef = doc(db, "tarefas", id);
+
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.data() === undefined) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    if (!snapshot.data()?.public) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    const milliseconds = snapshot.data()?.created?.seconds * 1000;
+
+    const task = {
+        tarefa: snapshot.data()?.tarefa,
+        public: snapshot.data()?.public,
+        created: new Date(milliseconds).toLocaleDateString(),
+        user: snapshot.data()?.user,
+        id: id,
+    }
+
+    console.log(task);
+
+    return {
+        props: {
+            item: task,
+        }
+    }
+}
